67c572606c9dc02e3f9cf6a5f6f99929264a294c
Lukas Wiedemann <lukaswiedemann@gmail.com>
2019-04-04 12:20:15 +0200

    Added foreign keys

--- Repository/Repository/Score.cs
+++ Repository/Repository/Score.cs
@@ -8,7 +8,11 @@ namespace Repository {
     class Score {
         public int Id { get; set; }
         public int Points { get; set; }
+
+        public int GameId { get; set; }
         public Game Game { get; set; }
+
+        public int UserId { get; set; }
         public User User { get; set; }
     }
 }
